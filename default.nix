7eb3056f9d909f9ca0ed1f9eef38d06a0428a38e
Michael Adler <therisen06@gmail.com>
2024-11-20 08:27:45 +0100

    darwin: try to fix it

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.11";
+  # nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.11";
+  nixpkgs = fetchTarball "https://github.com/michaeladler/nixpkgs/tarball/fix/opensc-darwin";
   pkgs = import nixpkgs { };
 in
 
